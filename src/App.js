3883069644bcf9f956d8a28c53b9def860ef4d2b
K1ngAng31 <angelsantanahernandez2@gmail.com>
2020-09-27 20:16:00 -0600

    Need to finish UI, add more questions

--- src/App.js
+++ src/App.js
@@ -14,6 +14,51 @@ function App() {
         {answerOptions: 'IDK...', isCorrect: false},
       ],
     },
+    // {
+    //   q_text: 'What is Python?',
+    //   quiz_answer: [
+    //     {answerOptions: 'A Snake', isCorrect: false},
+    //     {answerOptions: 'A coding language', isCorrect: true},
+    //     {answerOptions: 'A game', isCorrect: false},
+    //     {answerOptions: 'IDK...', isCorrect: false},
+    //   ],
+    // },
+    // {
+    //   q_text: 'What is Python?',
+    //   quiz_answer: [
+    //     {answerOptions: 'A Snake', isCorrect: false},
+    //     {answerOptions: 'A coding language', isCorrect: true},
+    //     {answerOptions: 'A game', isCorrect: false},
+    //     {answerOptions: 'IDK...', isCorrect: false},
+    //   ],
+    // },
+    // {
+    //   q_text: 'What is Python?',
+    //   quiz_answer: [
+    //     {answerOptions: 'A Snake', isCorrect: false},
+    //     {answerOptions: 'A coding language', isCorrect: true},
+    //     {answerOptions: 'A game', isCorrect: false},
+    //     {answerOptions: 'IDK...', isCorrect: false},
+    //   ],
+    // },
+    // {
+    //   q_text: 'What is Python?',
+    //   quiz_answer: [
+    //     {answerOptions: 'A Snake', isCorrect: false},
+    //     {answerOptions: 'A coding language', isCorrect: true},
+    //     {answerOptions: 'A game', isCorrect: false},
+    //     {answerOptions: 'IDK...', isCorrect: false},
+    //   ],
+    // },
+    // {
+    //   q_text: 'What is Python?',
+    //   quiz_answer: [
+    //     {answerOptions: 'A Snake', isCorrect: false},
+    //     {answerOptions: 'A coding language', isCorrect: true},
+    //     {answerOptions: 'A game', isCorrect: false},
+    //     {answerOptions: 'IDK...', isCorrect: false},
+    //   ],
+    // },
   ];
   const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
@@ -32,27 +77,39 @@ function App() {
 		}
 	};
   return (
-  		<div className='app'>
-  			{showScore ? (
-  				<div className='score-section'>
-  					You scored {score} out of {qq.length}
-  				</div>
-  			) : (
-  				<>
+    <React.Fragment>
+      <div className = "h1">
+        <h1> Python Quiz </h1>
+      </div>
+      <div className = 'li'>
+        <li>Simple Questions</li>
+        <li>Choose 1 of 4 options</li>
+        <li>Good Luck!</li>
+      </div>
+
+
+
+		<div className='app'>
+      <body>{
+        showScore ?
+        (<div className='score-section'>You scored {score} out of {qq.length}</div>)
+         :(<>
   					<div className='question-section'>
-  						<div className='question-count'>
-  							<span>Question {currentQuestion + 1}</span>/{qq.length}
-  						</div>
-  						<div className='question-text'>{qq[currentQuestion].q_text}</div>
+              <div className='question-text'>({currentQuestion + 1}/{qq.length}) {qq[currentQuestion].q_text}</div>
+              <footer align = "left" style={{color:"black"}}>Angel Santana 2020 </footer>
   					</div>
   					<div className='answer-section'>
   						{qq[currentQuestion].quiz_answer.map((answerOption) => (
-  							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+  							<button onClick={() =>handleAnswerOptionClick(answerOption.isCorrect)}>
+                  {answerOption.answerOptions}
+                </button>
   						))}
   					</div>
   				</>
   			)}
-  		</div>
+      </body>
+		</div>
+    </React.Fragment>
     );
 }
 
